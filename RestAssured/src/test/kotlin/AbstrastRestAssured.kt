fee460f02355e2a878395fbda718f8d9d7888841
David Tompkins <david.tompkins@slalom.comgit configgit commit -m COmmitgit config --global user.email david.tompkins@slalom.com>
2020-08-27 11:28:37 -0500

    removed typo

--- RestAssured/src/test/kotlin/AbstrastRestAssured.kt
+++ RestAssured/src/test/kotlin/AbstrastRestAssured.kt
@@ -16,7 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 abstract class AbstractRestAssured {
-0
+    
     @Before
     fun setUp() {
         /*
